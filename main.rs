8dcd695a8c5f21e465d4367d4d2f971fe5c7101a
Frank Mayer <gh@frankmayer.dev>
2026-09-25 14:31:22 +0200

    arrow

--- main.rs
+++ main.rs
@@ -4,7 +4,7 @@ fn main() {
             (true, true) => println!("FizzBuzz"),
             (true, false) => println!("Fizz"),
             (false, true) => println!("Buzz"),
-            (false, false) => println!("{number}"),
+            (false, false) => println!("-> {number}"),
         }
     }
 }
