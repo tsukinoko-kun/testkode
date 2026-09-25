38fcb42b3679d1a848436eaa7f962da8f0e55244
Frank Mayer <gh@frankmayer.dev>
2026-09-25 14:32:20 +0200

    Change FizzBuzz output to Fizzbuzz

--- main.rs
+++ main.rs
@@ -1,7 +1,7 @@
 fn main() {
     for number in 1..=100 {
         match (number % 3 == 0, number % 5 == 0) {
-            (true, true) => println!("FizzBuzz"),
+            (true, true) => println!("Fizzbuzz"),
             (true, false) => println!("Fizz"),
             (false, true) => println!("Buzz"),
             (false, false) => println!("-> {number}"),
